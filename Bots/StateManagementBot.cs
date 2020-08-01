dd2f45f66b82cdae551ff5fa78558ec0bd236fa9
Your Name <you@example.com>
2020-08-01 18:08:34 -0300

    Salvando dados do usuário e da conversa

---
+++ Bots/StateManagementBot.cs
@@ -0,0 +1,90 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+
+using bankChatBot.StateBot;
+
+namespace bankChatBot.Bots
+{
+    public class StateManagementBot : ActivityHandler
+    {
+        private BotState _conversationState;
+        private BotState _userState;
+
+        public StateManagementBot(ConversationState conversationState, UserState userState)
+        {
+            _conversationState = conversationState;
+            _userState = userState;
+        }
+
+        public override async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            await base.OnTurnAsync(turnContext, cancellationToken);
+
+            // Save any state changes that might have occurred during the turn.
+            await _conversationState.SaveChangesAsync(turnContext, false, cancellationToken);
+            await _userState.SaveChangesAsync(turnContext, false, cancellationToken);
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            await turnContext.SendActivityAsync("Olá, bem vindo. (StateManagement)");
+        }
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            // Get the state properties from the turn context.
+
+            var conversationStateAccessors = _conversationState.CreateProperty<ConversationData>(nameof(ConversationData));
+            var conversationData = await conversationStateAccessors.GetAsync(turnContext, () => new ConversationData());
+
+            var userStateAccessors = _userState.CreateProperty<UserProfile>(nameof(UserProfile));
+            var userProfile = await userStateAccessors.GetAsync(turnContext, () => new UserProfile());
+
+            if (string.IsNullOrEmpty(userProfile.Name))
+            {
+                // First time around this is set to false, so we will prompt user for name.
+                if (conversationData.PromptedUserForName)
+                {
+                    // Set the name to what the user provided.
+                    userProfile.Name = turnContext.Activity.Text?.Trim();
+
+                    // Acknowledge that we got their name.
+                    await turnContext.SendActivityAsync($"Obrigado {userProfile.Name}. Para ver os dados da conversa digita algo.");
+
+                    // Reset the flag to allow the bot to go through the cycle again.
+                    conversationData.PromptedUserForName = false;
+                }
+                else
+                {
+                    // Prompt the user for their name.
+                    await turnContext.SendActivityAsync($"Qual o seu nome?");
+
+                    // Set the flag to true, so we don't prompt in the next turn.
+                    conversationData.PromptedUserForName = true;
+                }
+            }
+            else
+            {
+                // Add message details to the conversation data.
+                // Convert saved Timestamp to local DateTimeOffset, then to string for display.
+                var messageTimeOffset = (DateTimeOffset) turnContext.Activity.Timestamp;
+                var localMessageTime = messageTimeOffset.ToLocalTime();
+                conversationData.Timestamp = localMessageTime.ToString();
+                conversationData.ChannelId = turnContext.Activity.ChannelId.ToString();
+
+                // Display state data.
+                await turnContext.SendActivityAsync($"{userProfile.Name} enviou: {turnContext.Activity.Text}");
+                await turnContext.SendActivityAsync($"Recebido em: {conversationData.Timestamp}");
+                await turnContext.SendActivityAsync($"Origem da mensagem: {conversationData.ChannelId}");
+            }
+        }
+    }
+}
+
---
+++ StateBot/UserProfile.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace bankChatBot.StateBot
+{
+    // Defines a state property used to track information about the user.
+    public class UserProfile
+    {
+        public string Name { get; set; }
+    }
+}
